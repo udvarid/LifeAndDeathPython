9d91ef152ab657a44ad9ffc5675b8dd852392538
udvari1dona324 <udvari.donat@telekom.hu>
2023-03-27 09:45:08 +0200

    learned a new cod about the canvas

--- main.py
+++ main.py
@@ -70,4 +70,29 @@ def frame_grid():
     window.mainloop()
 
 
-frame_grid()
+def canvas():
+    l = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    n = 100  # this is the length of the list l
+    lngt = 800 // n  # this is the dimension of the squares that I want
+
+    fen = tk.Tk()
+    fen.geometry("1200x800")
+
+    # I would like to create a table of 4 rows on canvas
+    # each row should contain 4 squares
+    can = tk.Canvas(fen, width=450, height=400, bg="lightblue")
+    can.pack(side=tk.LEFT)
+
+    for i in range(n):
+        y = i * lngt
+        for j in range(n):
+            x = j * lngt
+            can.create_rectangle(x, y, x + lngt, y + lngt, fill="red")
+
+    f = tk.Frame(fen, width=150, height=400, bg="lightcoral")
+    f.pack(side=tk.LEFT)
+
+    fen.mainloop()
+
+
+canvas()
