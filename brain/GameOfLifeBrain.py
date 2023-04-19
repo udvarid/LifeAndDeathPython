3ece09b0789cc6482d07cae1f1d34d2892a008e9
udvari1dona324 <udvari.donat@telekom.hu>
2023-04-19 14:18:41 +0200

    calculation is being created

--- brain/GameOfLifeBrain.py
+++ brain/GameOfLifeBrain.py
@@ -7,6 +7,14 @@ class Cell:
         self.y = y
         self.value = value
 
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Cell):
+            return (self.x == o.x) and (self.y == o.y) and (self.value == o.value)
+        else:
+            return False
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y, self.value))
 
 class GameOfLifeBrain:
 
@@ -63,6 +71,40 @@ class GameOfLifeBrain:
         return len(list(filter(lambda cell: cell.value == tribe, self.universe))) >= self.init_cells
 
     def calculate_next_step(self):
+        empty_neighbour_cells = self.get_empty_neighbours()
+        final_empty_neighbour_cells = set()
+        for empty in empty_neighbour_cells:
+            if self.cell_has_place(empty):
+                final_empty_neighbour_cells.add(empty)
+        cells_to_calculate = set()
+        cells_to_calculate.update(final_empty_neighbour_cells)
+        cells_to_calculate.update(self.universe)
+        my_array = self.get_empty_array()
+        for cell in self.universe:
+            my_array[int(cell.x)][int(cell.y)] = int(cell.value)
+        return my_array
+
+    def cell_has_place(self, empty):
+        for cell in self.universe:
+            if cell.x == empty.x and cell.y == empty.y:
+                return False
+        return True
+
+    def get_empty_neighbours(self):
+        empties = set()
+        for cell in self.universe:
+            empties.update(self.get_empty_neighbours_for_given_cell(cell))
+        return empties
+
+    def get_empty_neighbours_for_given_cell(self, cell):
+        empties = set()
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if not (i == 0 and j == 0) and 0 <= cell.x + i < self.size and 0 <= cell.y + j < self.size:
+                    empties.add(Cell(cell.x + i, cell.y + j, 0))
+        return empties
+
+    def get_empty_array(self):
         my_array = []
         n = self.size
         for i in range(n):
@@ -70,7 +112,4 @@ class GameOfLifeBrain:
             for j in range(n):
                 my_row.append(0)
             my_array.append(my_row)
-
-        for cell in self.universe:
-            my_array[int(cell.x)][int(cell.y)] = int(cell.value)
         return my_array
